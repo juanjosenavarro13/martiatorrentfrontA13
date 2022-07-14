68c570161ef0b728e1357870815ed90f75a6f69f
juanxo13597 <juanxo97master@gmail.com>
2022-07-14 19:20:32 +0200

    prueba titulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { environment } from 'src/environments/environment';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'martiatorrentfrontA13';
+  titleApp = environment.titleApp;
 }
