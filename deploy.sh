26caeb458d39ad76cfce1a47128da56da6b5ace0
juanxo13597 <juanxo97master@gmail.com>
2022-07-16 13:03:46 +0200

    echo

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,17 @@ rm /var/www/html/*.txt
 rm /var/www/html/*.html
 rm /var/www/html/*.js
 rm /var/www/html/*.css
+echo "Archivos eliminados"
 
 # generar nueva build
 cd /var/www/html/frontend/
 npm install
+echo "Instalacion de dependencias"
 npm update
+echo "Actualizacion de dependencias"
 npm run build
+echo "Generacion de build"
 
 # copiar la nueva build
 cp /var/www/html/frontend/dist/martiatorrentfront-a13/* /var/www/html/
+echo "Copia de build"
