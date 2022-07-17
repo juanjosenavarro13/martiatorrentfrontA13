4ac01cc5e011e91279c74e3b74ea6170835682cf
juanxo13597 <juanxo97master@gmail.com>
2022-07-17 09:35:50 +0200

    registro completado

--- src/app/pages/registropage/registropage.component.ts
+++ src/app/pages/registropage/registropage.component.ts
@@ -9,15 +9,29 @@ import { UsuarioHttpService } from 'src/app/services/usuario-http.service';
 })
 export class RegistropageComponent implements OnInit {
   usuario: usuarioModel;
+  registrado: boolean;
+  registradoError: boolean;
+  error: any;
   constructor(private usuarioHttp: UsuarioHttpService) {
     this.usuario = new usuarioModel(0, '', '', '', '', '', '', '', '');
+    this.registrado = false;
+    this.registradoError = false;
   }
 
   ngOnInit(): void {}
 
   public registro(usuario: usuarioModel) {
-    this.usuarioHttp.registro(usuario).subscribe(data => {
-      alert('registro correcto');
-    });
+    this.usuarioHttp.registro(usuario).subscribe(
+      data => {
+        this.registrado = true;
+        this.registradoError = false;
+      },
+      error => {
+        this.registradoError = true;
+        this.registrado = false;
+        this.error = error;
+        console.log(error);
+      }
+    );
   }
 }
