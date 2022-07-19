a4fd6779b4fd06c0b3c73f99a06b8da5dfc63da2
juanxo13597 <juanxo97master@gmail.com>
2022-07-19 17:17:30 +0200

    actualizacion email y rol fuera del modelo del servicio

--- src/app/pages/perfilpage/perfilpage.component.ts
+++ src/app/pages/perfilpage/perfilpage.component.ts
@@ -11,9 +11,11 @@ import { Component, OnInit } from '@angular/core';
 export class PerfilpageComponent implements OnInit {
   usuario: usuarioModel;
   loading: boolean;
+  update: boolean;
   constructor(private _usuarioService: UsuarioService, private _usuarioHttp: UsuarioHttpService) {
     this.usuario = {} as usuarioModel;
     this.loading = false;
+    this.update = false;
   }
 
   ngOnInit(): void {
@@ -31,9 +33,11 @@ export class PerfilpageComponent implements OnInit {
     this._usuarioService.usuario$.subscribe(usuario => {
       value.id = usuario.id;
       value.email = usuario.email;
-      console.log(value);
       this._usuarioHttp.perfilUpdate(token!, value).subscribe(data => {
-        console.log(data);
+        this.update = true;
+        setTimeout(() => {
+          this.update = false;
+        }, 3000);
       });
     });
   }
