768b44961e197b57ad51443fa462e43c77757381
juanxo13597 <juanxo97master@gmail.com>
2022-07-19 18:44:52 +0200

    deplo with assets

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ rm /var/www/html/*.txt
 rm /var/www/html/*.html
 rm /var/www/html/*.js
 rm /var/www/html/*.css
+rm -R /var/www/html/assets/
 echo "Archivos eliminados"
 
 # generar nueva build
@@ -19,5 +20,5 @@ npm run build
 echo "Generacion de build"
 
 # copiar la nueva build
-cp /var/www/html/frontend/dist/martiatorrentfront-a13/* /var/www/html/
+cp -R /var/www/html/frontend/dist/martiatorrentfront-a13/* /var/www/html/
 echo "Copia de build"
